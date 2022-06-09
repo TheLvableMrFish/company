82e951385ade4f5dd77e8a7f39c630b20921eaf1
unknown <paganochristians@gmail.com>
2022-06-09 17:56:36 -0500

    fix logo image not loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header =()=>{
             <nav className="navbar navbar-light navbar-expand-lg">
                 <div className="container">
                     <a className="navbar-brand nav-link-header" href="/company">
-                        <img className='img-fluid logo' src='/media/webLogo.png' />
+                        <img className='img-fluid logo' src={`./media/webLogo.png`} />
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
